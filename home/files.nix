7f8403e2229dd0f016cbbfed2b17e8ca9efcbf15
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-02-06 13:02:07 -0300

    feat: add lazygit files to resources

--- home/files.nix
+++ home/files.nix
@@ -3,6 +3,7 @@
   home.file.".config/hypr/hyprland.conf".source = ../res/home/hypr/hyprland.conf;
   home.file.".config/hypr/binds.conf".source = ../res/home/hypr/binds.conf;
   home.file.".config/kitty/kitty.conf".source = ../res/home/kitty.conf;
+  home.file.".config/lazygit/config.yml".source = ../res/home/lazygit/config.yml;
   home.file.".config/swaylock/config".source = ../res/home/swaylock;
   home.file.".config/swayidle/config".source = ../res/home/swayidle;
 }
