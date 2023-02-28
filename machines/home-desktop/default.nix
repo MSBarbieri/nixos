f984dbad1f2bb5a6d4d0201e8224f40eb9228ad6
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-02-28 14:31:02 -0300

    remove extra hosts

--- machines/home-desktop/default.nix
+++ machines/home-desktop/default.nix
@@ -21,13 +21,6 @@
   networking = {
     hostName = "home-desktop";
     extraHosts = ''
-      127.0.0.1 instagram.com
-      127.0.0.1 tiktok.com
-      # 127.0.0.1 youtube.com
-      # 127.0.0.1 www.youtube.com
-      # 127.0.0.1 twitch.tv
-
-      # 127.0.0.1 www.twitch.tv
 
     '';
   };
